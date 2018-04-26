338de1bbf29538c578680ba177d599f3f6266ced
Stefan Limbacher <stefan@stelim.de>
2018-04-26 23:37:32 +0200

    fixed pod syntax errors

--- lib/Map/Tube/Nuremberg.pm
+++ lib/Map/Tube/Nuremberg.pm
@@ -19,20 +19,29 @@ with 'Map::Tube';
 =head1 DESCRIPTION
 It currently provides functionality to find the shortest route between
 the two given stations. The map contains currently only (U-Bahn) subway stations.
+
 =head1 CONSTRUCTOR
+
     use Map::Tube::Nuremberg;
     my $tube = Map::Tube::Nuremberg->new;
+
 =head1 METHODS
+
 =head2 get_shortest_route(I<START>, I<END>)
+
 This method expects two parameters I<START> and I<END> station name.
 Station names are case insensitive. The station sequence from I<START>
 to I<END> is returned.
+
     use Map::Tube::Nuremberg;
     my $tube = Map::Tube::Nuremberg->new;
     my $route = $tube->get_shortest_route('Rathenauplatz', 'Hauptbahnhof');
     print "Route: $route\n";
+
 =head1 SEE ALSO
+
 L<Map::Tube>.
+
 =cut
 
 1;
